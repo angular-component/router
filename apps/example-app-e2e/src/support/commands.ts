712a71e8ecef3e7bbb21cfd24ac0c261ef1e9298
Brandon <robertsbt@gmail.com>
2020-07-19 14:25:00 -0500

    test: add E2E tests for example-app to test router functionality

--- apps/example-app-e2e/src/support/commands.ts
+++ apps/example-app-e2e/src/support/commands.ts
@@ -29,3 +29,20 @@ Cypress.Commands.add('login', (email, password) => {
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// TODO: uncomment once Applitools work properly
+// import '@applitools/eyes-cypress/commands';
+
+let LOCAL_STORAGE_MEMORY: any = {};
+
+Cypress.Commands.add('saveLocalStorage', () => {
+  Object.keys(localStorage).forEach((key) => {
+    LOCAL_STORAGE_MEMORY[key] = localStorage[key];
+  });
+});
+
+Cypress.Commands.add('restoreLocalStorage', () => {
+  Object.keys(LOCAL_STORAGE_MEMORY).forEach((key) => {
+    localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
+  });
+});
