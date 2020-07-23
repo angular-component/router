54e728becd7432134ffa5d848dcfc92fc135b461
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-07-24 03:02:17 +0530

    feat: ng-add schematics implementation (#16)
    Closes #9

---
+++ libs/angular-routing/schematics/ng-add/index.spec.ts
@@ -0,0 +1,62 @@
+import {
+  SchematicTestRunner,
+  UnitTestTree,
+} from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+import { RouterOptions } from './schema';
+
+export const workspaceOptions = {
+  name: 'workspace',
+  newProjectRoot: 'projects',
+  version: '6.0.0',
+  defaultProject: 'bar',
+};
+
+export const defaultAppOptions = {
+  name: 'bar',
+  inlineStyle: false,
+  inlineTemplate: false,
+  viewEncapsulation: 'Emulated',
+  routing: false,
+  style: 'css',
+  skipTests: false,
+};
+
+
+const collectionPath = path.join(__dirname, '../collection.json');
+
+describe('ng add function', () => {
+  let appTree: UnitTestTree;
+  let schematicRunner: SchematicTestRunner;
+
+  const defaultOptions: RouterOptions = {
+    project: 'bar',
+    module: 'app'
+  };
+
+  beforeEach(async () => {
+
+    schematicRunner = new SchematicTestRunner('angular-routing', collectionPath);
+
+    appTree = await schematicRunner.runExternalSchematicAsync(
+      '@schematics/angular',
+      'workspace',
+      workspaceOptions
+    ).toPromise();
+
+    appTree = await schematicRunner.runExternalSchematicAsync(
+      '@schematics/angular',
+      'application',
+      defaultAppOptions,
+      appTree
+    ).toPromise();
+  });
+
+  it('should import RouterModule a specified module', () => {
+    const options = { ...defaultOptions };
+
+    const tree = schematicRunner.runSchematic('ng-add', options, appTree);
+    const content = tree.readContent(`/projects/bar/src/app/app.module.ts`);
+    expect(content).toContain('RoutingModule.forRoot()');
+  });
+});
---
+++ libs/angular-routing/schematics/ng-add/schema.ts
@@ -0,0 +1,5 @@
+export interface RouterOptions {
+  project: string;
+  module?: string;
+  path?: string;
+}
