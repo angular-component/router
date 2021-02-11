ff59e38fa1180708e72c1a0c66d765439bc04912
Brandon <robertsbt@gmail.com>
2021-02-10 20:46:55 -0600

    chore: update lib name

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   projects: [
-    '<rootDir>/libs/angular-routing',
+    '<rootDir>/libs/router',
     '<rootDir>/apps/example-app',
     '<rootDir>/apps/recursive-routes',
     '<rootDir>/apps/responsive-app',
