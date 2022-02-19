e1eeaf01278e7f81db81d53ced3456e458b73ee3
Brandon Roberts <robertsbt@gmail.com>
2022-02-19 14:18:31 -0600

    chore: fix tests and formatting

--- libs/router/jest.config.js
+++ libs/router/jest.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  name: 'router',
+  displayName: 'router',
   preset: '../../jest.preset.js',
-  coverageDirectory: '../../coverage/libs/router',
-
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
-      stringifyContentPathRegex: '\\.(html|svg)$',
-
       tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  displayName: 'router',
+  coverageDirectory: '../../coverage/libs/router',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
-  transform: { '^.+\\.(ts|js|html)$': 'jest-preset-angular' },
 };
